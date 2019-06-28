efcaa1075295715fe1b946316408e0caf00d6143
Magus <mr.magus@gmail.com>
2019-06-27 23:27:27 -0500

    filtering; sorting; custom console/logging

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import './App.css';
 import CSVReader from './CSVReader/CSVReader';
 import TableRow from './TableRow';
 import RecordData from './records.js'
+import Dock from 'react-dock';
+import { Hook, Console, Decode } from 'console-feed'
+
+const styles = {
+  dockContent: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#242424',
+    // display: 'flex',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // flexDirection: 'column'
+  },
+  remove: {
+    position: 'absolute',
+    zIndex: 1,
+    right: '10px',
+    top: '10px',
+    cursor: 'pointer'
+  }
+}
 
 class App extends Component {
   constructor(props) {
@@ -10,7 +31,15 @@ class App extends Component {
     this.fileInput = React.createRef();
     this.fileName = "example.csv";
     this.state = {
-      data: RecordData
+      data: RecordData,
+      dock: {
+        isVisible: false,
+        fluid: true,
+        size: 0.25,
+        dimMode: "transparent",
+        customAnimation: false
+      },
+      logs: [],
     };
   }
 
@@ -25,22 +54,37 @@ class App extends Component {
         data: data.data
       });
     }
+    console.info(`Parsing ${this.fileName} complete.`)
+    console.info(`Total rows: ${this.state.data.length}`)
   }
 
   handleOnError = (err, file, inputElem, reason) => {
-    console.log(err);
+    console.error(err);
   }
 
   handleImportOffer = () => {
     this.fileInput.current.click();
   }
 
+  handleVisibleChange = isVisible => {
+    this.setState({ dock: {isVisible} });
+  }
+
+  componentDidMount() {
+    Hook(window.console, log => {
+      this.setState(({ logs }) => ({ logs: [...logs, Decode(log)] }))
+    })
+  }
+
   render() {
+    const duration = 200;
+
     return (
       <div>
         <TableRow
           data={this.state.data}
           fileName={this.fileName}
+          handleVisibleChange={this.handleVisibleChange}
         />
         <CSVReader
           onFileLoaded={this.handleReadCSV}
@@ -57,6 +101,28 @@ class App extends Component {
           }}
         />
         <button className="btn btn-primary" onClick={this.handleImportOffer}>Load CSV</button>
+        <Dock
+          position="bottom"
+          size={this.state.dock.size}
+          dimMode={this.state.dock.dimMode}
+          isVisible={this.state.dock.isVisible}
+          onVisibleChange={this.handleVisibleChange}
+          fluid={this.state.dock.fluid}
+          dimStyle={{ background: 'rgba(0, 0, 100, 0.2)' }}
+          dockStyle={null}
+          dockHiddenStyle={null}
+          duration={duration}
+        >
+          {() => {
+            return (
+            <div style={styles.dockContent}>
+              <div style={{ backgroundColor: '#242424' }}>
+                <Console logs={this.state.logs} variant="dark" />
+              </div>
+            </div>)
+          }
+          }
+        </Dock>
       </div>
     );
   }
--- src/TableRow.js
+++ src/TableRow.js
@@ -1,23 +1,100 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import { Grid, VirtualTable, TableHeaderRow, } from '@devexpress/dx-react-grid-material-ui';
+import { Plugin, Template, TemplatePlaceholder } from "@devexpress/dx-react-core";
+import { FilteringState, IntegratedFiltering, SortingState, IntegratedSorting } from '@devexpress/dx-react-grid';
+import { Grid, VirtualTable, TableHeaderRow, TableFilterRow, DragDropProvider, 
+         TableColumnReordering, TableColumnResizing, ColumnChooser, TableColumnVisibility, 
+         Toolbar,   
+       } from '@devexpress/dx-react-grid-material-ui';
+import DateRange from '@material-ui/icons/DateRange';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTerminal } from '@fortawesome/free-solid-svg-icons'
+
+const styles = theme => ({
+  tableStriped: {
+    '& tbody tr:nth-of-type(odd)': {
+      backgroundColor: fade(theme.palette.primary.main, 0.15),
+    },
+  },
+});
+
+const FilterIcon = ({ type, ...restProps }) => {
+  if (type === 'month') return <DateRange {...restProps} />;
+  return <TableFilterRow.Icon type={type} {...restProps} />;
+};
+
+const TableComponentBase = ({ classes, ...restProps }) => (
+  <VirtualTable.Table
+    {...restProps}
+    className={classes.tableStriped}
+  />
+);
+
+const Root = props => <Grid.Root {...props} style={{ height: '100%' }} />;
+export const TableComponent = withStyles(styles, { name: 'TableComponent' })(TableComponentBase);
 
 class TableRow extends React.PureComponent {
 
+  getCellValue(row, columnName) {
+    return columnName === "index"
+      ? this.state.data.indexOf(row)
+      : row[columnName];
+  }
+
+  getRowId = (row) => {
+    return this.props.data.indexOf(row);
+  }
+
     render() {
+      const CustomToolbarMarkup = () => {
+        const {
+          handleVisibleChange
+        } = this.props
+        return (
+        <Plugin name="customToolbarMarkup">
+          <Template name="toolbarContent">
+            <FontAwesomeIcon icon={faTerminal} className="btnConsole" onClick={() => handleVisibleChange(true)} />
+            <TemplatePlaceholder />
+          </Template>
+        </Plugin>)
+      };
+      const rows = this.props.data;
+      const columns = Object.keys(rows[0]).map(row => ({
+        name: row,
+        title: row,
+      }));
       return (
         <Paper>
           <Grid
-            rows={this.props.data.map((value, index) => (
-              Object.assign(value, {row: index})
-            ))}
-            columns={Object.keys(this.props.data[0]).map(row => ({
-              name: row,
-              title: row
-            }))}
+            rows={rows}
+            columns={columns}
+            getRowId={this.getRowId}
+            rootComponent={Root}
           >
-            <VirtualTable />
-            <TableHeaderRow />
+            <DragDropProvider />
+            <FilteringState defaultFilters={[]} />
+            <SortingState defaultSorting={[{ columnName: 'row', direction: 'asc' }]} />
+            <IntegratedFiltering />
+            <IntegratedSorting />
+            <VirtualTable 
+              height={"auto"}
+              tableComponent={TableComponent}
+            />
+            {/* <TableColumnResizing defaultColumnWidths /> */}
+            <TableHeaderRow showSortingControls />
+            <TableColumnReordering 
+              defaultOrder={columns.map(column => column.name)}
+            />
+            <TableFilterRow
+              showFilterSelector
+              iconComponent={FilterIcon}
+            />
+            <TableColumnVisibility />
+            <Toolbar />
+            <ColumnChooser />
+            <CustomToolbarMarkup />
           </Grid>
         </Paper>
       );
